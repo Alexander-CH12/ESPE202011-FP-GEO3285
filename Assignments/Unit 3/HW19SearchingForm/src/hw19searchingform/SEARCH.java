1f4690b9e5804a06c2d3ed5c9bc721d606b02c71
Alexander-CH12 <marco.chicaisa26@gmail.com>
2021-03-24 16:24:45 -0500

    WS25OperationsWhitStrings

---
+++ Assignments/Unit 3/HW19SearchingForm/src/hw19searchingform/SEARCH.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw19searchingform;
+
+/**
+ *
+ * @author qwe
+ */
+class SEARCH {
+
+    static int searchNumber(int[] numbers, int num) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static int searchNumber(int[] numbers) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
